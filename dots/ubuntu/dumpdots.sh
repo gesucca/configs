7ab167a6032bf4607b90a9583b47ced9846a903b
gesucca <simcip.ssdev@gmail.com>
2018-08-31 08:59:08 +0200

    Fuck me

--- dots/ubuntu/dumpdots.sh
+++ dots/ubuntu/dumpdots.sh
@@ -3,13 +3,13 @@
 cd /mnt/ramdisk
 git clone --depth=1 https://github.com/gesucca/configs.git
 
-yes | cp ~/Desktop/.loaddots.sh configs/dots/loaddots.sh
+yes | cp ~/Desktop/.loaddots.sh configs/dots/ubuntu/loaddots.sh
 
 yes | cp ~/Desktop/.github_sync.sh configs/repos/github_sync.sh
 yes | cp ~/Desktop/.work_on_repos.sh configs/repos/work_on_repos.sh
 
-yes | cp ~/.zshrc configs/dots/zshrc
-yes | cp ~/.vimrc configs/dots/vimrc
+yes | cp ~/.zshrc configs/dots/ubuntu/zshrc
+yes | cp ~/.vimrc configs/dots/ubuntu/vimrc
 
 cd configs
 git add .
--- dots/ubuntu/loaddots.sh
+++ dots/ubuntu/loaddots.sh
@@ -3,13 +3,13 @@
 cd /mnt/ramdisk
 git clone --depth=1 https://github.com/gesucca/configs.git
 
-yes | cp configs/dots/loaddots.sh ~/Desktop/.loaddots.sh
-yes | cp configs/dots/dumpdots.sh ~/Desktop/.dumpdots.sh
+yes | cp configs/dots/ubuntu/loaddots.sh ~/Desktop/.loaddots.sh
+yes | cp configs/dots/ubuntu/dumpdots.sh ~/Desktop/.dumpdots.sh
 
 yes | cp configs/repos/github_sync.sh ~/Desktop/.github_sync.sh
 yes | cp configs/repos/work_on_repos.sh ~/Desktop/.work_on_repos.sh
 
-yes | cp configs/dots/zshrc ~/.zshrc
-yes | cp configs/dots/vimrc ~/.vimrc
+yes | cp configs/dots/ubuntu/zshrc ~/.zshrc
+yes | cp configs/dots/ubuntu/vimrc ~/.vimrc
 
 yes | rm -rf configs
