4535173199868e3574510cc0b854290b2f2307a9
gesucca <simcip.ssdev@gmail.com>
2018-09-12 16:14:34 +0200

    Refined kde install - time to try!

--- setup/arch/kde.sh
+++ setup/arch/kde.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 sudo pacman -S plasma-meta
-sudo pacman -S terminology dolphin dolphin-plugins ark sweeper okular kwrite kfind kdf vlc
-
+sudo pacman -S kdegraphics-meta kdemultimedia-meta kdeutils-meta
+sudo pacman -S terminology dolphin kwrite kfind
 
 echo "RUN: systemctl enable sddm.service"
 
