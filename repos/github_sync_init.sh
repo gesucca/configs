94e5dcdc3c4ef5d4fef8977617295e664e19366b
gesucca <simcip.ssdev@gmail.com>
2018-11-09 17:12:47 +0100

    Added username on last repo

--- repos/github_sync_init.sh
+++ repos/github_sync_init.sh
@@ -18,5 +18,5 @@ git clone https://github.com/eviloutpost/eviloutpost.github.io
 
 # aem
 git clone https://SimoneCipriani@bitbucket.org/SimoneCipriani/notes.git
-git clone https://github.com/aem-progetti/tech-doc-gen.git
-git clone https://github.com/aem-progetti/nodejs-app-template.git
+git clone https://scipriani-aem@github.com/aem-progetti/tech-doc-gen.git
+git clone https://scipriani-aem@github.com/aem-progetti/nodejs-app-template.git
